f7e8705427ea844df5c4066be5e7d5a705b5c238
xamdp <64017084+xamdp@users.noreply.github.com>
2026-05-14 17:01:53 +0900

    wip: add constructor
    To visualize how to add eventlistener for passing arguments from dialog or modal to
    addBookToLibrary. Added a partial function that add books to the
    library.

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 const myLibrary = [];
 
-function Book() {
-	// constructor
+function Book(title, author, date, pages, read) {
+	if (!new.target) {
+		throw Error("You must use the 'new' operator to call the constructor");
+	}
+	this.title = title;
+	this.author = author;
+	this.date = date;
+	this.pages = pages;
+	this.read = read;
+	this.addBookTo = function() {
+		return `${this.title} ${this.author} ${this.date} ${this.pages}  ${this.read}`;
+	};
 }
 
-function addBookToLibrary() {
+// Book.prototype.addBookTo = function() {
+// 	console.log(`${this.title}`);
+// };
+
+// this is a sample object, to visualize how will i add eventlistener for passing arguments from dialog or modal to
+// addBookToLibrary.
+let book = new Book("Minna No Nihongo", "先生", "Today", 296, "not yet");
+
+// Object.getPrototypeOf(thisbook);
+// let val = thisbook.addBookTo();
+// console.log(val);
+
+function addBookToLibrary(title, author, date, pages, read) {
+	let book = new Book(title, author, date, pages, read);
+	myLibrary.push(book);
 	// take params, create a book then store it in the array
 }
