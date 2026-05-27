e187249dd6826b5f31d90ba04c8b05eff8a6130a
xamdp <64017084+xamdp@users.noreply.github.com>
2026-05-28 06:23:54 +0900

    fix: display existing books in array once

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const myLibrary = [
-	// {
-	// 	title: "Harry Potter",
-	// 	author: "J.K Rowling",
-	// },
-	// {
-	// 	title: "The Road to React",
-	// 	author: "Robin Wieruch",
-	// },
+	{
+		id: 1,
+		title: "Harry Potter",
+		author: "J.K Rowling",
+	},
+	{
+		id: 2,
+		title: "The Road to React",
+		author: "Robin Wieruch",
+	},
 ];
 
 // Book constructor for making book objects
-function Book(title, author, pages, read) {
+function Book(id, title, author, pages, read) {
 	if (!new.target) {
 		throw Error("You must use the 'new' operator to call the constructor");
 	}
@@ -42,18 +44,35 @@ bookForm.addEventListener("submit", (event) => {
 // this is partially working, but should display book info in a book card element or something
 // basically i need to create new elements under .book-card
 
+const bookdisplay = document.querySelector(".book-display");
+const book_author = document.querySelector("#book-author");
+const book_pages = document.querySelector("#book-pages");
+
 function showBooks() {
 	myLibrary.forEach((book) => {
-		const bookdisplay = document.querySelector(".book-display");
-		const bookCard = document.createElement("div");
-		const title = document.createElement("p");
-		title.textContent = book.title;
+		// i found out that if i click the My books it creates another exising book card.
+		if (document.getElementById(book.id)) {
+			return;
+		}
 
-		const author = document.createElement("p");
-		author.textContent = book.author;
+		// create book card
+		console.log(book);
+		const bookCard = document.createElement("div");
 		bookCard.classList.add("book-card");
+		bookCard.setAttribute("id", book.id);
 
 		bookdisplay.appendChild(bookCard);
-		bookCard.append(title, author);
+
+		// create title p tag
+		const book_title = document.createElement("p");
+		book_title.classList.add("book-title");
+		book_title.textContent += book.title;
+
+		// create author p tag
+		const book_author = document.createElement("p");
+		book_author.classList.add("book-author");
+		book_author.textContent += book.author;
+
+		bookCard.append(book_title, book_author);
 	});
 }
