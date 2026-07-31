b77e74351fc0b2def639385e152a51316e73289c
xamdp <64017084+xamdp@users.noreply.github.com>
2026-07-31 15:58:00 +0900

    ref: refactor core library logic
    Overall, I learned a lot by using a class, instead of using plain object
    constructors.
    
    - Moved the library array inside the constructor
    - properties which are used for object creation, are directly queried by
      querySelectors.
    - added initListeners, like a central listener method that waits for any
      events (submit, click, etc)
    - main class methods, are renderBookCard and handleBookForm.
    - I don't know yet, how to actually use the stopListener.

--- script.js
+++ script.js
@@ -1,48 +1,113 @@
 
-let myLibrary = [
-	{
-		id: 1,
-		title: "中級から学ぶ日本語",
-		author: "KENKYUSHA",
-		pages: 144,
-		read: "Yes, I have!",
-	},
-	{
-		id: 2,
-		title: "The Road to React",
-		author: "Robin Wieruch",
-		pages: 250,
-		read: "Nawp, Not yet",
-	},
-	{
-		id: 3,
-		title: "よつばと！",
-		author: "Kiyohiko Azuma",
-		pages: 1000,
-		read: "Yes, I have!",
-	},
-];
-
 class Library {
-	constructor(id, title, author, pages, read) {
-		if (!new.target) {
-			throw Error("You must use thee 'new' operator to call the constructor.")
-		}
+	constructor() {
 
-		this.id = id;
-		this.title = title;
-		this.author = author;
-		this.pages = pages;
-		this.read = read;
-		this.addBookto = function() {
-			return `${this.id} ${this.title} ${this.author} ${this.pages} ${this.read}`;
-		}
+		this.template = document.getElementById('book-card-template')
+		this.bookDisplay = document.querySelector('.book-display')
+		this.form = document.querySelector("#bookForm");
+
+		this.id = crypto.randomUUID();
+		this.title = document.querySelector("#book_title");
+		this.author = document.querySelector("#book_author");
+		this.pages = document.querySelector("#book_pages");
+		this.read = document.getElementsByName('status');
+
+		this.renderBtn = document.getElementById('renderBtn');
+
+
+		// the library array itself is moved inside the constructor
+		this.myLibrary = [
+			{
+				id: 1,
+				title: "中級から学ぶ日本語",
+				author: "KENKYUSHA",
+				pages: 144,
+				read: "Yes, I have!",
+			},
+			{
+				id: 2,
+				title: "The Road to React",
+				author: "Robin Wieruch",
+				pages: 250,
+				read: "Nawp, Not yet",
+			},
+			{
+				id: 3,
+				title: "よつばと！",
+				author: "Kiyohiko Azuma",
+				pages: 1000,
+				read: "Yes, I have!",
+			},
+		];
+
+
 	}
 
+	// i tried searching, on where to actually put eventListeners
+	// this one initialize a listener
+	initListeners() {
+		this.form.addEventListener('submit', this.handleBookForm);
+		this.renderBtn.addEventListener('click', this.renderBookCard);
+	}
+
+	// while this one, removes, idk yet, when to call this
+	stopListeners() {
+		this.form.removeEventListener('submit', this.handleBookForm)
+	}
+
+	handleBookForm = (event) => {
+		event.preventDefault();
+
+		const checkedRadio = document.querySelector(
+			'input[name="status"]:checked')
+
+		const readValue = checkedRadio ? checkedRadio.value : 'not read';
+
+		const newBookObj = {
+			id: this.id,
+			title: this.title.value,
+			author: this.author.value,
+			pages: this.pages.value,
+			read: readValue,
+		};
+
+		console.log(newBookObj);
+
+		this.myLibrary.push(newBookObj);
+		this.renderBookCard();
+		this.form.reset();
+	}
+
+
 	// class methods
-	createBookCard() { }
-	showBooks() { }
+	renderBookCard = () => {
+
+		this.myLibrary.forEach((book) => {
+
+			// idk if i should use id or data-book-id 
+			if (document.getElementById(book.id)) {
+				return;
+			}
+
+			const notYetcard = this.template.content.cloneNode(true)
+			const bookCardDiv = notYetcard.querySelector('.book-card');
+			bookCardDiv.setAttribute('id', book.id)
+
+			notYetcard.querySelector('.book-title').textContent = book.title;
+			notYetcard.querySelector('.book-author').textContent = book.author
+			notYetcard.querySelector('.book-pages').textContent = book.pages;
+			notYetcard.querySelector('.book-status').textContent = book.read;
+
+			this.bookDisplay.append(notYetcard);
+		})
+
+	}
+
 	deleteBook() { }
 
 
 }
+
+const mylib = new Library();
+mylib.renderBookCard();
+mylib.initListeners();
